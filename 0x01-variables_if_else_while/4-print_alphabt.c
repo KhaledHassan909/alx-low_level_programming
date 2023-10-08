d0818c80694898de15576b5c169317eaabb0f0e4
KhaledHassan909 <Khaled.kh.hasssan@gmail.com>
2023-10-08 22:16:19 +0300

    Added

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: 'Printing alphabet'
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char ch;
+ch = 'a';
+while (ch <= 'z')
+{
+if (ch == 'q' || ch == 'e')
+{
+    ch++;
+    continue;
+}
+else
+{
+    putchar(ch);
+    ch++;
+}
+}
+putchar('\n');
+return (0);
+}
