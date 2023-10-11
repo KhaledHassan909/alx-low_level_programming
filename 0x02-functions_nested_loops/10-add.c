f5aab430c3a05196395ca6a5e4fceae2ac177f72
KhaledHassan909 <Khaled.kh.hasssan@gmail.com>
2023-10-11 15:44:45 +0300

    Task10 added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Calculate sum of 2 integers
+ * @n1: 1st integer input
+ * @n2: 2nd integer input
+ *
+ * Return: int sum
+ */
+int add(int n1, int n2)
+{
+return (n1 + n2);
+}
