b4fde3030a67dfc11a6fa0df48f242f5c0d4851a
KhaledHassan909 <Khaled.kh.hasssan@gmail.com>
2023-10-11 05:52:24 +0300

    Task 1 Added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* print_alphabet - void function
+*
+* Description: 'Printing lowercase alphabet'
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+char c;
+c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
+
+/**
+* main - Entry point
+*
+* Description: 'Printing _putchar'
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+print_alphabet();
+return (0);
+}
