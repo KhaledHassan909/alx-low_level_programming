fb21a15b49bf9296aabeaacf3767b840af8efd4f
KhaledHassan909 <Khaled.kh.hasssan@gmail.com>
2023-10-12 19:15:35 +0300

    Task5 added

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numers 0-14 x10 times
+ *
+ * Return: None
+ */
+void more_numbers(void)
+{
+int n;
+int iter;
+while (iter <= 10)
+{
+n = 0;
+while (n <= 14)
+{
+if (n < 10)
+_putchar('0' + n);
+else
+{
+_putchar('0' + n / 10);
+_putchar('0' + n % 10);
+}
+n++;
+}
+_putchar('\n');
+iter++;
+}
+}
