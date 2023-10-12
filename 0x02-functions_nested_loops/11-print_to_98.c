57431f3ff08a50e86ddcb6e63bb5bc60df98858f
KhaledHassan909 <Khaled.kh.hasssan@gmail.com>
2023-10-12 14:31:56 +0300

    Task0 added

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,59 +6,59 @@ if (n < 98)
 {
 while (n < 99)
 {
-if( n < 10 && n >= 0)
-{
+if (n < 98 && n >= 10){
+_putchar('0' + n / 10);
+_putchar('0' + n % 10);
+_putchar(',');
+_putchar(' ');
+}
+else if (n < 10 && n >= 0){
 _putchar('0' + n);
 _putchar(',');
 _putchar(' ');
 }
-else if (n >= 10 && n < 98)
-{
-_putchar('0' + n / 10);
-_putchar('0' + n % 10);
-_putchar(',');
-_putchar(' '); 
-}
-else if ( n <= -10)
-{
-_putchar('0' + n / 10);
-_putchar('0' + n % 10);
+else if (n < 0 && n >= -9){
+_putchar('0' + n);
 _putchar(',');
 _putchar(' ');
 }
 else
 {
-_putchar('0' + n);
+_putchar('0' + n / 10);
+_putchar('0' + n % 10);
 _putchar(',');
 _putchar(' ');
+}
 }
 n++;
-}
-}
+}  
 else if (n > 98)
 {
-while (n > 99)
+while (n > 98)
 {
-if( n < 10)
+if (n >= 100)
 {
-_putchar('0' + n);
+_putchar(n / 100 + '0');
+_putchar((n / 10) % 10 + '0');
+_putchar(n % 10 + '0');
 _putchar(',');
 _putchar(' ');
 }
-else
-{
+else if (n >= 100 && n < 1000){
 _putchar('0' + n / 10);
 _putchar('0' + n % 10);
 _putchar(',');
 _putchar(' ');
+}
 }
 n--;
 }
-}
-else
+else 
 {
 _putchar('0' + n / 10);
 _putchar('0' + n % 10);
+_putchar(',');
+_putchar(' ');
 }
 _putchar('\n');
 }
