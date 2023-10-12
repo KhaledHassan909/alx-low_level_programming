c50ced829a1f47bb938133eee59a7272de884412
KhaledHassan909 <Khaled.kh.hasssan@gmail.com>
2023-10-12 20:30:26 +0300

    Task6 modified

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,17 +9,16 @@
 void print_line(int n)
 {
 int iter = 0;
-while (iter <= n)
+if (n <= 0)
+_putchar('\n');
+while (iter < n)
 {
 if (n <= 0)
-{
-_putchar('\n');
 break;
-}
 else
 _putchar('_');
 iter++;
 }
-if (n != 0)
+if (n != 0 && n > 0)
 _putchar('\n');
 }
