38b1be080e73e94e2c3fa66b094b414f456b5bbb
KhaledHassan909 <Khaled.kh.hasssan@gmail.com>
2023-10-16 10:49:41 +0300

    Task1 added

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Change the value of an integer to 98.
+ * @a: 1st integer to be swaped.
+ * @b: 2nd integer to be swaped.
+ */
+void swap_int(int *a, int *b)
+{
+int tmp;  /* temproral variable */
+tmp = *a; /* temproral variable holds value of *a */
+*a = *b;  /* assigning value of *b in *a */
+*b = tmp; /* assiginig value of *a in *b */
+}
+
+
