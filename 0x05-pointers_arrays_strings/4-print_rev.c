4549ed40278f15cc69c2bd82f534d2d27be99af0
KhaledHassan909 <Khaled.kh.hasssan@gmail.com>
2023-10-16 12:38:57 +0300

    Task4 added

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - Printing string in reverse order.
+ * @s: Pointer to string.
+ */
+void print_rev(char *s)
+{
+int i = strlen(s);
+for (; i >= 0; i--)
+_putchar(s[i]);
+_putchar('\n');
+}
