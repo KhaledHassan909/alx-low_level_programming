04e5f2f73cb29b9191f62bd0c519e6123fe49a44
KhaledHassan909 <Khaled.kh.hasssan@gmail.com>
2023-10-16 15:47:54 +0300

    Task6 modified

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,10 +1,14 @@
 #include "main.h"
 #include <string.h>
 
+/**
+ * puts2 - printing every other char of string
+ * @str: input string
+ */
 void puts2(char *str)
 {
 int i = 0;
-int str_len = strlen(str);
+int str_len = strlen(str)-1;
 _putchar(str[0]);
 for(; i < str_len; i=i+2)
 _putchar(str[i+2]);
