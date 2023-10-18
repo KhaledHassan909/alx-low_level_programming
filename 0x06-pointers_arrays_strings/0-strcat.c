300f834da4f3dfe7fa8862197728c0751f85f56e
KhaledHassan909 <Khaled.kh.hasssan@gmail.com>
2023-10-18 10:22:24 +0300

    Task0 added

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strcat - concatenates two strings by appending src to dest.
+ * @dest: string to append to.
+ * @src:  string to be appended.
+ * Return: concantenated string.
+ */
+char *_strcat(char *dest, char *src)
+{
+return (strcat(dest, src));
+}
