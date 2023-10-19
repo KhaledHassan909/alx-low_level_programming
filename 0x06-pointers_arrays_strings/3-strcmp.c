f7f0b5e911c20dc5330bc3f38993f1d8a0836fe6
KhaledHassan909 <Khaled.kh.hasssan@gmail.com>
2023-10-19 15:12:31 +0300

    Task3 modified

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,15 +12,15 @@ int i = 0;
 while(s1[i] != '\0' && s2[i] != '\0')
 {
 if(s1[i] < s2[i])
-return -1;
+return -15;
 else if(s1[i] > s2[i])
-return 1;
+return 15;
 i++;
 }
 if(s1[i] == '\0' && s2[i] == '\0')
 return 0;
 else if(s1[i] == '\0')
-return -1;
+return -15;
 else
-return 1;
+return 15;
 }
