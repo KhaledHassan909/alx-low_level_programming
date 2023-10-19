d618cead87df799f4b8b5a95066a7a56ba455949
KhaledHassan909 <Khaled.kh.hasssan@gmail.com>
2023-10-19 15:06:03 +0300

    Task3 modified

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,23 +9,16 @@
 int _strcmp(char *s1, char *s2)
 {
 int i = 0;
-while (s1[i] != '\0' && s2[i] != '\0') {
-if (s1[i] < s2[i]) {
-return -15;
-}
-else if (s1[i] > s2[i]) {
-return 15;
-}
-i++;
-}
-if (s1[i] == '\0' && s2[i] == '\0') {
+int j = 0;   
+while(s1[i] != '\0')
+i++;     
+while(s2[j] != '\0')
+j++;   
+if (i == 0 || j == 0) {
 return 0;
-}
-else if (s1[i] == '\0') {
-return -15;
 }
 else
 {
-return 15;
-}
+return (s1[i - 1] - s2[j - 1]);
+}   
 }
