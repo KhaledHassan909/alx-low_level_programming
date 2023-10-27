531c7269e7c59927221e7d9c8769c0b9801718ce
KhaledHassan909 <Khaled.kh.hasssan@gmail.com>
2023-10-27 15:33:14 +0200

    Task4 added

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,7 +11,19 @@
 int main(int argc, char *argv[])
 {
 int add = 0;
-int i;
+int i, j;
 for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (argv[i][j] < '0' || argv[i][j] > '9')
+{
+printf("ERROR\n");
+return (1);
+}
+}
+add += atoi(argv[i]);
+}
+printf("%d\n", add);
 return (0);
 }
