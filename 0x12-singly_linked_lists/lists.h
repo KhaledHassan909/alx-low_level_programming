b35c9c146f6d7e145f959396798480aa48fccc5e
KhaledHassan909 <Khaled.kh.hasssan@gmail.com>
2023-11-16 12:17:35 +0200

    Header file modified

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,11 +3,19 @@
 
 #include <stdio.h>
 
-typedef struct list_t
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
 {
     char *str;
-    int len;
-    struct list_t *next;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
