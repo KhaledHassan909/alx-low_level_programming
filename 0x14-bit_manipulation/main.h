6ee5a08218a6472e4034994b892444e29fdbceb3
KhaledHassan909 <Khaled.kh.hasssan@gmail.com>
2023-11-25 23:29:49 +0200

    Task1 added

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,7 +6,9 @@
 #include <string.h>
 
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 int char_to_int(char c);
 int _strlen(const char *str);
+int _putchar(char c);
 
 #endif
