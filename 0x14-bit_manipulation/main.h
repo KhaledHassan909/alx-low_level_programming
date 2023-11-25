d223c396750541d13b34b6b67ee9386f7c51e53d
KhaledHassan909 <Khaled.kh.hasssan@gmail.com>
2023-11-26 00:45:28 +0200

    Header file modified

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -10,5 +10,6 @@ void print_binary(unsigned long int n);
 int char_to_int(char c);
 int _strlen(const char *str);
 int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
 
 #endif
